2a2cae8b511a5c52de9e299dc7b028634eeef7ea
Paul solomon <your-email@example.com>
2025-10-02 15:08:32 -0400

    updated docs in rag pipeline for pricing model

--- frontend/pages/pricing.js
+++ frontend/pages/pricing.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import ChatJarvis from '../components/ChatJarvis';
 import PricingGroup from '../components/PricingGroup';
 import PlanTypeSelector from '../components/Molecules/PlanTypeSelector';
 import PricingToggle from '../components/Atoms/PricingToggle';
@@ -60,6 +61,7 @@ export default function Pricing() {
             />
           </div>
         </section>
+        <ChatJarvis />
       </Layout>
     </>
   );
